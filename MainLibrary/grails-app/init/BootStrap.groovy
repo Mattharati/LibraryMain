4ffdc58432444e8cac8636b57b7c7a27cb53b3b0
Student <b3028830@my.shu.ac.uk>
2016-04-01 01:25:17 +0100

    added 3 instances in the bootstrap file of the librarian

--- MainLibrary/grails-app/init/BootStrap.groovy
+++ MainLibrary/grails-app/init/BootStrap.groovy
@@ -1,6 +1,27 @@
+import com.MainLibrary.*
+
 class BootStrap {
 
     def init = { servletContext ->
+
+   def librarian1 = new Librarian( name: 'jeff',
+                                  email: 'jeff@hotmail.co.uk',
+                               username: 'jeff123',
+                               password: 'randyjeff1',
+                              telephone: '0114 1234567').save()
+
+   def librarian2 = new Librarian( name: 'Sarlene',
+                                  email: 'saralene@hotmail.co.uk',
+                               username: 'sarlene0',
+                               password: 'sarlene123',
+                              telephone: '0114 7654321').save()
+
+   def librarian3 = new Librarian( name: 'Will',
+                                  email: 'Will@hotmail.co.uk',
+                               username: 'will23',
+                               password: 'will2323',
+                              telephone: '0114 1243567').save()
+
     }
     def destroy = {
     }
