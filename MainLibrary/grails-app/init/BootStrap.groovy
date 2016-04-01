4324f9cfc08ad9d3fa8e8a9e067bd42dcf17ca7e
Student <b3028830@my.shu.ac.uk>
2016-04-01 02:10:14 +0100

    added 3 instances in the bootstrap file for all the different classes and added details too

--- MainLibrary/grails-app/init/BootStrap.groovy
+++ MainLibrary/grails-app/init/BootStrap.groovy
@@ -22,6 +22,81 @@ class BootStrap {
                                password: 'will2323',
                               telephone: '0114 1243567').save()
 
+   def course1 = new Course(      title: 'Computing',
+                                  tutor: 'Candice',
+                                   code: 'com123',
+                             department: 'ASEC',
+                            description: 'The computing department').save()
+
+   def course2 = new Course(      title: 'History',
+                                  tutor: 'Lesley',
+                                   code: 'his123',
+                             department: 'Arts',
+                            description: 'The department of arts').save()
+
+   def course3 = new Course(      title: 'Physics',
+                                  tutor: 'Phillip',
+                                   code: 'phy123',
+                             department: 'Sciences',
+                            description: 'Department of Sciences').save()
+
+   def student1 = new Student(     name: 'Joe',
+                                  email: 'joe@hotmail.co.uk',
+                              studentId: 'joe123',
+                                 course: 'Computing').save()
+
+   def student2 = new Student(     name: 'Liam',
+                                  email: 'liam@hotmail.co.uk',
+                              studentId: 'liam123',
+                                 course: 'Physics').save()
+
+   def student3 = new Student(     name: 'Frank',
+                                  email: 'frank@hotmail.co.uk',
+                              studentId: 'frank123',
+                                 course: 'History').save()
+
+    def library1 = new Library( location: 'Sheffield',
+                            openingHours: '24/7',
+                                    book: 'Of Mice And Men',
+                                 student: 'Cody',
+                               librarian: 'Jeff').save()
+ 
+    def library2 = new Library( location: 'Manchester',
+                            openingHours: '24/7',
+                                    book: 'The Fury',
+                                 student: 'Steve',
+                               librarian: 'Peter').save()
+ 
+    def library3 = new Library( location: 'Glasgow',
+                            openingHours: '24/7',
+                                    book: 'Loch Ness Myths',
+                                 student: 'Jimmy',
+                               librarian: 'Gregory').save()
+
+   def book1 = new Book(          title: 'Gatsby',
+                                 author: 'The Great Gatsby',
+                                   isbn: '123456789',
+                           dateBorrowed: new Date('20/06/2015'),
+                             returnDate: new Date('17/07/2016'),
+                                student: 'Frank').save()
+
+   def book2 = new Book(          title: 'Harry Potter',
+                                 author: 'that woman',
+                                   isbn: '987654321',
+                           dateBorrowed: new Date('10/01/2014'),
+                             returnDate: new Date('12/11/2015'),
+                                student: 'Liam').save()
+
+   def book3 = new Book(          title: 'The Fury',
+                                 author: 'Some Angry Fella',
+                                   isbn: '918273645',
+                           dateBorrowed: new Date('10/01/2013'),
+                             returnDate: new Date('13/02/2014'),
+                                student: 'Joe').save()
+
+
+
+
     }
     def destroy = {
     }
