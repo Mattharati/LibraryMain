44d2794957715726cfb8f122d7add2f7fc36fbce
Student <b3028830@my.shu.ac.uk>
2016-04-01 05:40:01 +0100

    added more relationships and fixed error with grails-app

--- MainLibrary/grails-app/init/BootStrap.groovy
+++ MainLibrary/grails-app/init/BootStrap.groovy
@@ -4,98 +4,107 @@ class BootStrap {
 
     def init = { servletContext ->
 
-   def librarian1 = new Librarian( name: 'jeff',
-                                  email: 'jeff@hotmail.co.uk',
-                               username: 'jeff123',
-                               password: 'randyjeff1',
-                              telephone: '0114 1234567').save()
+   def librarian1 = new Librarian( name: 'Andy',
+                                  email: 'andy@library.com',
+                               username: 'andy123',
+                               password: 'kingandy1',
+                              telephone: '0114 2778976').save()
 
-   def librarian2 = new Librarian( name: 'Sarlene',
-                                  email: 'saralene@hotmail.co.uk',
-                               username: 'sarlene0',
-                               password: 'sarlene123',
-                              telephone: '0114 7654321').save()
+   def librarian2 = new Librarian( name: 'Sarah',
+                                  email: 'sarah@library.com',
+                               username: 'sarah_30',
+                               password: 'sarah101',
+                              telephone: '0114 2732576').save()
 
-   def librarian3 = new Librarian( name: 'Will',
-                                  email: 'Will@hotmail.co.uk',
-                               username: 'will23',
-                               password: 'will2323',
-                              telephone: '0114 1243567').save()
+   def librarian3 = new Librarian( name: 'Matt',
+                                  email: 'matt@library.com',
+                               username: 'matty21',
+                               password: 'Matt01',
+                              telephone: '0114 2932572').save()
 
    def course1 = new Course(      title: 'Computing',
-                                  tutor: 'Candice',
-                                   code: 'com123',
-                             department: 'ASEC',
-                            description: 'The computing department').save()
+                                  tutor: 'Stephen Lofthouse',
+                                   code: 'Com123',
+                             department: 'ACES',
+                            description: 'Variety of modules to enhance your technical ability!').save()
 
    def course2 = new Course(      title: 'History',
-                                  tutor: 'Lesley',
-                                   code: 'his123',
+                                  tutor: 'Tom Oxspring',
+                                   code: 'His123',
                              department: 'Arts',
-                            description: 'The department of arts').save()
+                            description: 'In-depth study of eras to enhance your historical knowledge!').save()
 
    def course3 = new Course(      title: 'Physics',
-                                  tutor: 'Phillip',
-                                   code: 'phy123',
+                                  tutor: 'Alex Swinden',
+                                   code: 'Phy123',
                              department: 'Sciences',
-                            description: 'Department of Sciences').save()
+                            description: 'Mind blowing practicals in top quality labs!').save()
 
-   def student1 = new Student(     name: 'Joe',
-                                  email: 'joe@hotmail.co.uk',
-                              studentId: 'joe123',
-                                 course:  course2).save()
-
-   def student2 = new Student(     name: 'Liam',
-                                  email: 'liam@hotmail.co.uk',
-                              studentId: 'liam123',
-                                 course:  course1).save()
-
-   def student3 = new Student(     name: 'Frank',
-                                  email: 'frank@hotmail.co.uk',
-                              studentId: 'frank123',
+   def student1 = new Student(     name: 'Will',
+                                  email: 'will@student.com',
+                              studentId: 'b4025516',
                                  course:  course3).save()
 
-    def library1 = new Library( location: 'Sheffield',
-                            openingHours: '24/7',
-                                    book: 'Of Mice And Men',
-                                 student: 'Cody',
-                               librarian:  librarian1).save()
- 
-    def library2 = new Library( location: 'Manchester',
-                            openingHours: '24/7',
-                                    book: 'The Fury',
-                                 student: 'Steve',
-                               librarian:  librarian2).save()
- 
-    def library3 = new Library( location: 'Glasgow',
-                            openingHours: '24/7',
-                                    book: 'Loch Ness Myths',
-                                 student: 'Jimmy',
-                               librarian:  librarian3).save()
+   def student2 = new Student(     name: 'Harry',
+                                  email: 'Harry@student.com',
+                              studentId: 'b4037745',
+                                 course:  course1).save()
 
-   def book1 = new Book(          title: 'Gatsby',
-                                 author: 'The Great Gatsby',
-                                   isbn: '123456789',
-                           dateBorrowed:  new Date('20/06/2015'),
-                             returnDate:  new Date('17/07/2016'),
-                                student:  student1).save()
+   def student3 = new Student(     name: 'James',
+                                  email: 'james@student.com',
+                              studentId: 'b4061532',
+                                 course:  course2).save()
 
-   def book2 = new Book(          title: 'Harry Potter',
-                                 author: 'that woman',
-                                   isbn: '987654321',
-                           dateBorrowed:  new Date('10/01/2014'),
-                             returnDate:  new Date('12/11/2015'),
-                                student:  student2).save()
+   def book1 = new Book(          title: 'Lord of The Flies',
+                                 author: 'William Golding',
+                                   isbn: '0-3234-3267-7',
+                           dateBorrowed:  new Date('10/01/2016'),
+                             returnDate:  new Date('17/01/2016'),
+                                student: 'Will').save()
 
-   def book3 = new Book(          title: 'The Fury',
-                                 author: 'Some Angry Fella',
-                                   isbn: '918273645',
-                           dateBorrowed:  new Date('10/01/2013'),
-                             returnDate:  new Date('13/02/2014'),
-                                student:  student3).save()
+   def book2 = new Book(          title: 'Advanced PHP',
+                                 author: 'Gerry Form',
+                                   isbn: '0-5733-7934-5',
+                           dateBorrowed:  new Date('02/12/2015'),
+                             returnDate:  new Date('14/01/2016'),
+                                student: 'James').save()
 
+   def book3 = new Book(          title: 'Hogwarts: A History',
+                                 author: 'Albus Dumbledore',
+                                   isbn: '0-6398-2865-3',
+                           dateBorrowed:  new Date('29/02/2016'),
+                             returnDate:  new Date('06/03/2016'),
+                                student: 'Harry').save()
 
+   def library1 = new Library( location: 'Adsetts',
+                           openingHours: '24/7',
+                                   book:  book3,
+                                student:  student3,
+                              librarian:  librarian3).save()
 
+   def library2 = new Library( location: 'Collegiate',
+                           openingHours: '24/7',
+                                   book:  book1,
+                                student:  student1,
+                              librarian:  librarian1).save()
+
+   def library3 = new Library( location: 'Owen',
+                           openingHours: '24/7',
+                                   book:  book2,
+                                student:  student2,
+                              librarian:  librarian2).save()
+
+                           library1.addToLibrarians(librarian3)
+                           library2.addToLibrarians(librarian1)
+                           library3.addToLibrarians(librarian2)
+
+                           library1.addToStudents(student3)
+                           library2.addToStudents(student1)
+                           library3.addToStudents(student2)
+
+                           library1.addToBooks(book3)
+                           library2.addToBooks(book1)
+                           library3.addToBooks(book2)
 
     }
     def destroy = {
